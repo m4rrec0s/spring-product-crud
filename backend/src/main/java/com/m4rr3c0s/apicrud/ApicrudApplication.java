2c5d2b17efe17240f46124430339b0c32e204fe0
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-12-03 16:31:44 -0300

    feat: Add initial Spring Boot project configuration with Maven

---
+++ backend/src/main/java/com/m4rr3c0s/apicrud/ApicrudApplication.java
@@ -0,0 +1,13 @@
+package com.m4rr3c0s.apicrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicrudApplication.class, args);
+	}
+
+}
